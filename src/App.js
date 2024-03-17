dcc398547643c25207bd0e2f84dce7f097a9b160
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-03-17 22:20:55 +0100

    add project assets

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Main from "./pages/Main";
 
 function App() {
   return (
     <div className="App">
+      <Main />
     </div>
   );
 }
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Navbar from "../components/navbar/Navbar";
 
 const Main = () => {
-  return <div>Main</div>;
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 };
 
 export default Main;
