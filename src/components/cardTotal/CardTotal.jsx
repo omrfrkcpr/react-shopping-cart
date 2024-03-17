e7a3cd14b53f6561ca9e9c2de3f5c26d07f30fb5
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-03-17 13:27:33 +0100

    chore: add project components and its files

---
+++ src/components/cardTotal/CardTotal.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./CardTotal.css";
+
+const CardTotal = () => {
+  return <div>CardTotal</div>;
+};
+
+export default CardTotal;
---
+++ src/components/productForm/ProductForm.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./ProductForm.css";
+
+const ProductForm = () => {
+  return <div>ProductForm</div>;
+};
+
+export default ProductForm;
